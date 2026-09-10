5759ef51e456cea829dbcff7e485986823f205ae
janhvibhawar <janhvibhawar28@gmail.com>
2026-09-10 20:24:07 +0530

    Added swapping two numbers problem

---
+++ 06_swap_numbers.py
@@ -0,0 +1,9 @@
+# fifth
+# write a program to swap two variables without a third variable , using arithemethic operations
+# example : before swap a = 10, b = 20
+# after swap a = 20, b = 10
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+a,b = b,a
+print("a =", a , "b =", b)
