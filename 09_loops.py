b29eccf6499f07c3be763e7953a7088711a7a6e0
janhvibhawar <janhvibhawar28@gmail.com>
2026-09-15 10:05:05 +0530

    Print table of 5

--- 09_loops.py
+++ 09_loops.py
@@ -1,3 +1,11 @@
-while True:
-    string = input("Enter a string:")
-    print(string)
+# password match
+correct_pass = "123"
+not_found = True
+
+while not_found:
+    passw = input("Enter password:")
+    if passw == correct_pass:
+        not_found = False
+
+print("Password Matched")
+
---
+++ 10_tables.py
@@ -0,0 +1,7 @@
+#tables
+
+i = 1
+while i<=10:
+    print(i*5)
+    i+=1
+ 
